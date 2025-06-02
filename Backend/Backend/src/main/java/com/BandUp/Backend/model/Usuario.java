e8b7327eb4067d7a4a19c87458c5a985b789c124
Augusto Cáceres <168226091+M3nch0@users.noreply.github.com>
2025-06-02 18:19:04 -0300

    Add files via upload

---
+++ Backend/Backend/src/main/java/com/BandUp/Backend/model/Usuario.java
@@ -0,0 +1,20 @@
+package com.BandUp.Backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String nombre;
+    private String apellido;   
+}
